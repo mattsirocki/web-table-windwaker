a972d8dce6c0c5291805df9d06a23351f35945a8
Matt Sirocki <mattsirocki@github.com>
2013-08-25 11:13:25 -0400

    Initial Commit

---
+++ build.php
@@ -0,0 +1,202 @@
+<?php
+
+/*
+ * The purpose of this script is to generate the #windwaker table.
+ */
+
+require_once '../Web Table/Source/Web/Table/Autoloader.php';
+require_once 'islands.php';
+
+$table = new \Web\Table\Table('windwaker', 'Dark', true);
+$table->alias = 'The Legend of Zelda: The Wind Waker';
+
+// Group Names
+$groups = array
+(
+    'Fishman'          => 'Fishman',
+    'Heart Containers' => 'Heart Container',
+    'Pieces of Heart'  => 'Piece of Heart',
+    'Sunken Treasures' => 'Sunken Treasure',
+    'Wind Waker Songs' => 'Wind Waker Song',
+    'Required Items'   => 'Required Item',
+    'Optional Items'   => 'Optional Item',
+    'Triforce Charts'  => 'Triforce Chart',
+    'Treasure Charts'  => 'Treasure Chart',
+    'Other Charts'     => 'Other Chart',
+    'Big Octos'        => 'Big Octo',
+    'Blue ChuChus'     => 'Blue ChuChu',
+    'Great Fairies'    => 'Great Fairy',
+    'Mini-Games'       => 'Mini-Game',
+    'Pirate Platforms' => 'Pirate Platform',
+    'Secret Caves'     => 'Secret Cave',
+    'Side Quests'      => 'Side Quest',
+    'Submarines'       => 'Submarine',
+    'Tingle Quests'    => 'Tingle Quest',
+    'Other Secrets'    => 'Other Secret',
+    'Quest Log'        => 'Quest Log',
+    'Information'      => 'Information',
+);
+
+// Initialize Counters
+$totals = array();
+foreach (array_keys($groups) as $key)
+    $totals[$key] = 0;
+
+foreach ($islands as $ri => $r)
+{
+    $row = $table->appendRow('(' . ($ri + 1) . ')');
+    foreach ($r as $ci => $c)
+    {
+        $cell = $row->appendCell($ci);
+
+        $items = array();
+
+        $deltas = array();
+        foreach (array_keys($groups) as $key)
+            $deltas[$key] = 0;
+
+        foreach ($c as $gi => $g)
+        {
+            if (empty($g))
+                continue;
+
+            $group = $cell->appendGroup($gi);
+
+            foreach ($g as $ii => $i)
+            {
+                $totals[$gi]++;
+                $deltas[$gi]++;
+
+                switch ($gi)
+                {
+                    case 'Fishman':
+                        $items[] = $group->appendItem('', $i, false);
+                        break;
+                    case 'Heart Containers':
+                        $items[] = $group->appendItem("$groups[$gi] #$totals[$gi]", $i, true);
+                        break;
+                    case 'Pieces of Heart':
+                        $items[] = $group->appendItem("$groups[$gi] #$totals[$gi]", $i, true);
+                        break;
+                    case 'Sunken Treasures':
+                        $items[] = $group->appendItem("$groups[$gi] #$totals[$gi] ($i[0])", $i[1], true);
+                        break;
+                    case 'Wind Waker Songs':
+                        $items[] = $group->appendItem($i[0], isset($i[1]) ? $i[1] : '', true);
+                        break;
+                    case 'Required Items':
+                        $items[] = $group->appendItem($i[0], $i[1], true);
+                        break;
+                    case 'Optional Items':
+                        $items[] = $group->appendItem($i[0], $i[1], true);
+                        break;
+                    case 'Triforce Charts':
+                        $items[] = $group->appendItem($i[0], $i[1], true);
+                        break;
+                    case 'Treasure Charts':
+                        $items[] = $group->appendItem($i[0], $i[1], true);
+                        break;
+                    case 'Other Charts':
+                        $items[] = $group->appendItem($i[0], $i[1], true);
+                        break;
+                    case 'Big Octos':
+                        $items[] = $group->appendItem("$groups[$gi] #$totals[$gi] ($i[0] Eyes)", $i[1], true);
+                        break;
+                    case 'Blue ChuChus':
+                        $items[] = $group->appendItem("$groups[$gi] #$totals[$gi]", $i, true);
+                        break;
+                    case 'Great Fairies':
+                        $items[] = $group->appendItem("$groups[$gi] #$totals[$gi]", $i, true);
+                        break;
+                    case 'Mini-Games':
+                        $items[] = $group->appendItem($i[0], $i[1], true);
+                        break;
+                    case 'Pirate Platforms':
+                        $items[] = $group->appendItem("$groups[$gi] #$totals[$gi]", $i, true);
+                        break;
+                   case 'Secret Caves':
+                       $items[] = $group->appendItem("$groups[$gi] #$totals[$gi]", $i, true);
+                       break;
+                   case 'Side Quests':
+                       $items[] = $group->appendItem($i[0], $i[1], true);
+                       break;
+                   case 'Submarines':
+                       $items[] = $group->appendItem("$groups[$gi] #$totals[$gi]", $i, true);
+                       break;
+                   case 'Tingle Quests':
+                       $items[] = $group->appendItem($i[0], $i[1], true);
+                       break;
+                   case 'Other Secrets':
+                       $items[] = $group->appendItem($i[0], $i[1], true);
+                       break;
+                   case 'Quest Log':
+                       $items[] = $group->appendItem($i[0], $i[1], true);
+                       break;
+                   case 'Information':
+                       $items[] = $group->appendItem('', $i, false);
+                       break;
+                }
+            }
+        }
+
+        // Relative Numbering
+        foreach ($items as $item)
+        {
+            preg_match_all('((' . implode('|', $groups) . ') ##([0-9]+))', $item->text, $matches);
+
+            for ($i = 0; $i < count($matches[0]); $i++)
+            {
+                $group = array_search($matches[1][$i], $groups);
+                $index = $totals[$group] - ($deltas[$group] - 1) + ($matches[2][$i] - 1);
+
+                $item->text = str_replace($matches[0][$i], $matches[1][$i] . ' #' . $index, $item->text);
+            }
+        }
+    }
+}
+
+$row = $table->appendRow('Nintendo Gallery');
+$special = array('!', '~', '*', '+');
+
+foreach ($gallery as $ci => $c)
+{
+    $cell = $row->appendCell($ci . ' (x' . count($c) . ')');
+    $group = $cell->appendGroup('');
+
+    foreach ($c as $di => $d)
+    {
+        $name = ('#' . ($di + 1)) . ' ' . array_shift($d);
+        $note = '';
+
+
+        if (strlen($d[0]) && $d[0][0] === '!')
+            $name = "<span style=\"color:red;\">$name</span>";
+
+        foreach ($d as $ei => &$e)
+            if (strlen($e) && in_array($e[0], $special))
+                $e = "<b>$e</b>";
+            else
+                $e = "<span style=\"font-size:smaller;\">$e</span>";
+
+        $group->appendItem($name, '<br />' . join('<br />', $d), true);
+    }
+
+    /*
+    for ($i = 0; $i < count($c); $i++)
+    {
+        $text = '';
+        foreach ($c[$i] as $j => $k)
+            if ($j > 0)
+                if (strlen($k) && in_array($k[0], array('!', '~', '*')))
+                    $text .= '<b>' . $k . '</b>';
+                else
+                    $text .= '<br /><span style="font-size: smaller;">' . $k . '</span>';
+
+        $group->appendItem('#' . ($i+1) . ' ' . $c[$i][0], $text, true);
+    }
+    */
+}
+
+echo $table->save('.') . NL;
+
+?>
---
+++ update.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * Part of Web Table (Windwaker)
+ *
+ * Read through a *.tbl file and preserve all checked/unchecked states. This
+ * assumes that new fields are only ever appended and that names (i.e.
+ * identifiers) remain constant.
+ *
+ * PHP 5.3
+ *
+ * @package   Console\Parser
+ * @author    Matt Sirocki <mattsirocki@gmail.com>
+ * @copyright 2012-2013, Matt Sirocki
+ * @license   MIT License
+ * @version   SVN: $Revision: 13 $
+ * @since     0.1.0 Initial Release
+ * @filesource
+ */
+
+$json_old = @file_get_contents($argv[1]);
+$json_new = @file_get_contents($argv[2]);
+$data_old = json_decode($json_old, true);
+$data_new = json_decode($json_new, true);
+$save = array();
+
+foreach ($data_old['rows'] as $row)
+{
+    $ri = $row['index'];
+    foreach ($row['cells'] as $cell)
+    {
+        $ci = $cell['index'];
+        foreach ($cell['groups'] as $group)
+        {
+            $gi = $group['index'];
+            foreach ($group['items'] as $item)
+            {
+                $ii = $item['index'];
+
+                $save[$ri][$ci][$gi][$ii] = $item['completed'];
+            }
+        }
+    }
+}
+
+foreach ($data_new['rows'] as &$row)
+{
+    $ri = $row['index'];
+    foreach ($row['cells'] as &$cell)
+    {
+        $ci = $cell['index'];
+        foreach ($cell['groups'] as &$group)
+        {
+            $gi = $group['index'];
+            foreach ($group['items'] as &$item)
+            {
+                $ii = $item['index'];
+
+                try
+                {
+                    if (@$save[$ri][$ci][$gi][$ii])
+                        echo "Completing {$item['name']}\n";
+                    @$item['completed'] = $save[$ri][$ci][$gi][$ii];
+                }
+                catch(Exception $e)
+                {
+                }
+            }
+        }
+    }
+}
+
+@file_put_contents($argv[1], json_encode($data_new));
